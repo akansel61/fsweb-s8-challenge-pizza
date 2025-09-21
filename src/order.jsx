02ae329c1fad764b9468a77de8fbf0f06f9979f6
ali akansel alioğlu <ali.akansel@hotmail.com>
2025-09-21 15:06:27 +0300

    Update order.jsx

--- src/order.jsx
+++ src/order.jsx
@@ -1,4 +1,18 @@
+import React, {useState} from "react";  
+
+
+
 export default function Order() {
+  const [pizza, setPizza] = useState({
+    const [pizza, setPizza] = useState({
+    name: 'Position Absolute Acı Pizza',
+    price: 85.50,
+    rating: 4.9,
+    reviews: 200,
+  });
+  });
+
+
   return (
     <div style={{ textAlign: "center", marginTop: "50px" }}>
       <h1>Pizza Sipariş Formu</h1>
