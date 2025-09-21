690e3600353348db0e40f5f0908c8d6f16160dd3
ali akansel alioğlu <ali.akansel@hotmail.com>
2025-09-21 14:55:04 +0300

    Create order.jsx

---
+++ src/order.jsx
@@ -0,0 +1,8 @@
+export default function Order() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Pizza Sipariş Formu</h1>
+      <p>Buraya form gelecek...</p>
+    </div>
+  );
+}
