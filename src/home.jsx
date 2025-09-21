61d59af213d4fda50db71495fd9853135f1e2489
ali akansel alioğlu <ali.akansel@hotmail.com>
2025-09-21 16:02:25 +0300

    Update home.jsx

--- src/home.jsx
+++ src/home.jsx
@@ -1,8 +1,31 @@
 import { Link } from "react-router-dom";
+import React from "react";
+import './home.css';
+import anaSayfaBanner from './images/iteration-1-images/home-banner.png';
 
 export default function Home() {
+   
+  };
+
+
   return (
-    <div style={{ textAlign: "center", marginTop: "50px" }}>
+    <div style={{
+    textAlign: "center", 
+    marginTop: "50px", 
+    backgroundImage: `url(${anaSayfaBanner})`,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    backgroundRepeat: 'no-repeat',
+    minHeight: '100vh',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    color: 'white',
+    fontFamily: 'Arial, sans-serif',
+    position: 'relative'
+    }}>
+
       <h1>🍕 Teknolojik Yemekler</h1>
       <p>Hoş geldiniz! Hemen kendi pizzanızı oluşturun.</p>
       <Link to="/order">
@@ -10,4 +33,4 @@ export default function Home() {
       </Link>
     </div>
   );
-}
+
