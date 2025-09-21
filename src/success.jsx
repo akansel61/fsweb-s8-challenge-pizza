f737c7b2800a82a31d11abef68e9ce6025ff1944
ali akansel alioğlu <ali.akansel@hotmail.com>
2025-09-21 14:48:32 +0300

    Create success.jsx

---
+++ src/success.jsx
@@ -0,0 +1,8 @@
+export default function Success() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>🎉 Siparişiniz Alındı!</h1>
+      <p>Teşekkürler, pizzanız yolda 😋</p>
+    </div>
+  );
+}
